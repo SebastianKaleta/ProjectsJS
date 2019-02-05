13126303f4182b1b26cf9652dfdff2773c198cb4
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-05 23:16:16 +0100

    Update

--- Project2/main.js
+++ Project2/main.js
@@ -1,4 +1,32 @@
 const squere = document.createElement('div');
 document.body.appendChild(squere);
-squere.style.width = '100px';
-squere.style.height = '100px';
+let grow = true;
+let size = 100; //size squere
+
+squere.style.width = size + 'px';
+squere.style.height = size + 'px';
+
+//max size squere
+// window.innerWidth*0.5
+
+window.addEventListener('scroll', function () {
+
+    if (size >= window.innerWidth / 2) {
+        grow = !grow;
+    } else if (size == 100) {
+        grow = !grow;
+    }
+
+    if (grow) {
+        size += 5;
+        squere.style.width = size + "px";
+        squere.style.height = size + "px";
+    } else {
+        size -= 5;
+        squere.style.width = size + "px";
+        squere.style.height = size + "px";
+    }
+
+
+    console.log(size);
+})
