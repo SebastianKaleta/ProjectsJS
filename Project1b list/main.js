2ed59782674588d9b9bc852c131beca3585ede21
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-05 22:07:23 +0100

    Update

--- Project1b list/main.js
+++ Project1b list/main.js
@@ -1,9 +1,10 @@
 let number = 1;
 const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const list = document.querySelector('ul');
 btn.addEventListener('click', function () {
     const newLi = document.createElement('li');
     newLi.textContent = number;
-    const list = document.querySelector('ul');
     list.appendChild(newLi);
 
 
