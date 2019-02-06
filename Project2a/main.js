a0a3c6247e771237b96826abe184ea18002cdd2e
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-06 22:05:02 +0100

    Update with coments

--- Project2a/main.js
+++ Project2a/main.js
@@ -3,17 +3,17 @@ let grow = true;
 
 
 document.body.style.height = "10000px";
-
+//create element "div" in DOM and add on end body
 const slider = document.createElement('div');
 document.body.appendChild(slider);
-
+//css properties only for training JS
 slider.style.backgroundColor = "green";
 slider.style.top = 0;
 slider.style.left = 0;
 slider.style.width = "100%";
 slider.style.height = size + "px";
 slider.style.position = "fixed";
-
+//fuction check event action "scroll" for window(all page what we see in frame browser), and add boolean property for variable
 window.addEventListener('scroll', function changeHeight() {
     if (size >= window.innerHeight / 2) {
         grow = !grow;
@@ -21,7 +21,7 @@ window.addEventListener('scroll', function changeHeight() {
         grow = !grow;
 
     }
-
+    //for true and false we set value which should change for action
     if (grow) {
         size += 5;
         slider.style.backgroundColor = "green";
@@ -31,6 +31,6 @@ window.addEventListener('scroll', function changeHeight() {
         slider.style.backgroundColor = "red";
         slider.style.height = size + "px";
     }
-    console.log(window.innerHeight);
+    console.log(window.innerHeight); //check value innerHeight for window
 
 });
