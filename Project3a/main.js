63b2159e29ac2a32932e16a4a469b907a48b70a7
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-07 23:05:19 +0100

    Update with coments

---
+++ Project3a/main.js
@@ -0,0 +1,28 @@
+// tutaj rozwiązanie
+
+
+const btn = document.querySelector('button');
+let size = 16;
+let click = 0;
+let list = [...document.querySelectorAll('li')];
+// list = document.querySelector('li');
+
+btn.addEventListener('click', function () {
+    click++;
+    //for loop
+    // for (let i = 0; i < list.length; i++) {
+    //     list[i].style.display = "block";
+    //     console.log(list[i]);
+    //     if (click >= 2) {
+    //         list[i].style.fontSize = size + "px";
+    //     }
+    list.forEach(function (list) {
+        list.style.display = "block";
+        if (click >= 2) {
+            list.style.fontSize = size + "px";
+        }
+    });
+
+    size++; //iteration outside of loop, becouse inside iterating every li separately
+
+});
