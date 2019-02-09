b465f3db32acfc1919d306e6ab61beec1a50dad7
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-09 23:23:12 +0100

    Create button js

--- Project3b/main.js
+++ Project3b/main.js
@@ -3,6 +3,10 @@ let orderElement = 1;
 
 const init = () => {
     //kod
+    const btn = document.createElement("button"); //create button
+    const textnode = document.createTextNode("Dodaj li"); //create text node with text inside button
+    btn.appendChild(textnode); //add variable with text to button
+    document.body.appendChild(btn); //add button to body
 }
 
 const createLiElements = () => {
