95c74fa784cbcd5eea04afa897f42319cd33e4ab
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-11 00:47:57 +0100

    Update solution

--- Project5 Change bg color on click/main.js
+++ Project5 Change bg color on click/main.js
@@ -1,20 +1,29 @@
 document.body.addEventListener('click', (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
 
-    const red = x / width * 100;
-    const green = y / height * 100;
-    const blue = ((x / width * 100 + y / height * 100)) / 2;
-
-    if ((x && y) == "even") {
-        document.body.style.backgroundColor = "red";
-    } else if ((x && y) == "odd") {
-        document.body.style.backgroundColor = "blue";
-    } else if ((x || y) == "odd") {
-        document.body.style.backgroundColor = "green";
+    if ((x % 2 === 0) && (y % 2 === 0)) {
+        document.body.style.backgroundColor = `red`;
+    } else if ((x % 2 === 1) && (y % 2 === 1)) {
+        document.body.style.backgroundColor = `blue`;
+    } else if ((x % 2 === 1) || (y % 2 === 1)) {
+        document.body.style.backgroundColor = `green`;
     }
 
     console.log(x, y)
+    //__________________________________________________
+    //ANOTHER SOLUTION
+    // const color = getColor(event);
+    // document.body.style.backgroundColor = color;
 });
+
+const getColor = (e) => {
+    if ((x % 2 === 0) && (y % 2 === 0)) {
+        return "red";
+    } else if ((x % 2 === 1) && (y % 2 === 1)) {
+        return "blue";
+    } else if ((x % 2 === 1) || (y % 2 === 1)) {
+        return "green"
+    }
+}
+// ___________________________________________________
