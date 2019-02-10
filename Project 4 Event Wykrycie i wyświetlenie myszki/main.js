f2fe257f5891031e3da15029e40f35db18820eb9
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-10 23:50:43 +0100

    Add backgroundColor on mouse move

--- Project 4 Event Wykrycie i wyświetlenie myszki/main.js
+++ Project 4 Event Wykrycie i wyświetlenie myszki/main.js
@@ -1,6 +1,10 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', (e) => {
     h1.textContent = `${e.clientX}, ${e.clientY}`; //client is browser
+
     // h1.textContent = `${e.pageX}, ${e.pageY}`; we can use "page", and it's mean our page document, not window
     // h1.textContent = `${e.screenX}, ${e.screenY}`; this means, the entire(cały) screen our computer
+
+    document.body.style.backgroundColor = `rgb(${e.clientX/3},${e.clientY/2},${e.clientX/e.clientY*20})`; //in RGB insert value from clientX and clientY (from 0 to 255 for RGB colors)
+
 })
