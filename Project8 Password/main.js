1aa876c4770997fb94202c3f59ba912e75650bdb
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-12 19:02:27 +0100

    Starting files Password app

--- Project8 Password/main.js
+++ Project8 Password/main.js
@@ -1,23 +1,28 @@
 const input = document.getElementById('pass');
 
 const div = document.querySelector('div');
-const password = "user";
-const message = "Wyjechałam na zawsze";
+const passwords = ["user", "wiosna"];
+const messages = ["Wyjechałam na zawsze", "Piękna pora roku"];
 
 input.addEventListener('input', (e) => {
+    div.textContent = "";
     // console.log(e.target.value);
-    if (password === e.target.value) {
-        div.textContent = message;
-        e.target.value = '';
-    } else {
-        div.textContent = "";
-    }
+    const text = e.target.value;
 
-})
+    passwords.forEach((password, index) => {
+        if (password === text) {
+            div.textContent = messages[index];
+            e.target.value = "";
+        }
+    });
 
-input.addEventListener('focus', (e) => {
-    e.target.classList.add('active');
-})
-input.addEventListener('blur', (e) => {
-    e.target.classList.remove('active');
-})
+
+
+    input.addEventListener('focus', (e) => {
+        e.target.classList.add('active');
+    });
+    input.addEventListener('blur', (e) => {
+        e.target.classList.remove('active');
+    });
+
+});
