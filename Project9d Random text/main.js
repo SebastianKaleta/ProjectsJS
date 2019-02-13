fd4ab38d72cb7561068340763c6fd8fe53b396f6
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-13 23:49:40 +0100

    Finish mini app Update

--- Project9d Random text/main.js
+++ Project9d Random text/main.js
@@ -1,32 +1,24 @@
-const options = ["Wykonaj", "Przemyśl to zanim przejdziesz do dzialania", "Życie jest zbyt krótkie, by myśleć!"];
+const options = ["Wykonaj", "Przemyśl to zanim przejdziesz do dzialania", "Życie jest zbyt krótkie by myśleć!"];
 
 const showAdArr = ["Walcz!", " Nie wiem!", "Wymyśl coś!"];
 
 const h1 = document.querySelector('h1');
-const btnAdd = document.querySelector('.add');
-const btnClean = document.querySelector('.clean');
-const btnShow = document.querySelector('.showAdvice');
-const btnOpp = document.querySelector('.showOptions');
 const input = document.querySelector('input');
 
 const addOption = (e) => {
     e.preventDefault();
-
     options.push(input.value);
-    console.log(options);
     input.value = "";
 }
 
 const cleanOption = (e) => {
     e.preventDefault();
     options.length = 0;
-    console.log(options);
 }
 
 const showAdvice = (e) => {
     e.preventDefault();
     h1.textContent = `${showAdArr[Math.floor(Math.random() * showAdArr.length)]}`;
-    console.log(h1);
 }
 
 const showOpp = (e) => {
@@ -37,7 +29,7 @@ const showOpp = (e) => {
 
 
 
-btnAdd.addEventListener('click', addOption);
-btnClean.addEventListener('click', cleanOption);
-btnShow.addEventListener('click', showAdvice);
-btnOpp.addEventListener('click', showOpp);
+document.querySelector('.add').addEventListener('click', addOption);
+document.querySelector('.clean').addEventListener('click', cleanOption);
+document.querySelector('.showAdvice').addEventListener('click', showAdvice);
+document.querySelector('.showOptions').addEventListener('click', showOpp);
