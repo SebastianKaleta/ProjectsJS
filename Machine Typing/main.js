5ce0fbd972bd9e6b57ecad6350b69aeca4a75eb8
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-17 23:50:49 +0100

    Machine typing

---
+++ Machine Typing/main.js
@@ -0,0 +1,35 @@
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['Hi ;)', 'This is machine typing.', 'See you leater!']
+
+// Parametry
+let activeLetter = -15;
+let activeText = 0;
+
+// Implementacja
+const addLetter = () => {
+    if (activeLetter >= 0) {
+        spnText.textContent += txt[activeText][activeLetter];
+    }
+
+    activeLetter++;
+    if (activeLetter === txt[activeText].length) {
+        activeText++;
+        if (activeText === txt.length) return;
+
+        return setTimeout(() => {
+            activeLetter = -15;
+            spnText.textContent = "";
+            addLetter();
+        }, 2000)
+
+    }
+    setTimeout(addLetter, 100);
+}
+
+addLetter();
+
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
