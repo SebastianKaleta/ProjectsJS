a0025129ec945669c69e05593209d879852bce9f
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-18 00:05:33 +0100

    Machine typing

--- Machine Typing/main.js
+++ Machine Typing/main.js
@@ -3,24 +3,24 @@ const spnCursor = document.querySelector('.cursor');
 const txt = ['Hi ;)', 'This is machine typing.', 'See you leater!']
 
 // Parametry
-let activeLetter = -15;
+let activeLetter = -15; //opóźnienie wywołania
 let activeText = 0;
 
 // Implementacja
 const addLetter = () => {
-    if (activeLetter >= 0) {
+    if (activeLetter >= 0) { //wywołanie z opóźnienia
         spnText.textContent += txt[activeText][activeLetter];
     }
 
     activeLetter++;
-    if (activeLetter === txt[activeText].length) {
+    if (activeLetter === txt[activeText].length) { //iteracja
         activeText++;
-        if (activeText === txt.length) return;
+        if (activeText === txt.length) return; //iteracja tablicy z przerwaniem po 3 indexie[2]
 
         return setTimeout(() => {
-            activeLetter = -15;
-            spnText.textContent = "";
-            addLetter();
+            activeLetter = -15; //opóźnienie wywołania kolejnych słów
+            spnText.textContent = ""; //zerowanie kontentu po 1 słowie
+            addLetter(); //ponowne wywołanie funkcji dla całej tablicy
         }, 2000)
 
     }
