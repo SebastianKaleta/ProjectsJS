2941b6c40298b573d80d5c442671fbc9f8d3aa39
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-18 20:44:01 +0100

    Baner dots

--- Banner/main.js
+++ Banner/main.js
@@ -14,11 +14,19 @@ const slideList = [{
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')];
 
 //Interfejs
 
 const time = 5000; //5s
 let active = 0;
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active')
+    dots[active].classList.add('active');
+}
+
 const changeSlide = () => {
     active++;
     if (active === slideList.length) {
@@ -26,6 +34,9 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img;
     h1.textContent = slideList[active].text;
-
+    changeDot();
 }
+
+
+
 setInterval(changeSlide, time);
