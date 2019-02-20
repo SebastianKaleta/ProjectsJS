46f223b6b5a96050224c150dd93b1e9bb595fb10
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-20 23:53:10 +0100

    Add task and remove with array

--- ToDoList/ToDO-Add task/main.js
+++ ToDoList/ToDO-Add task/main.js
@@ -1,31 +1,47 @@
+const toDoList = [];
+
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
-const taskNumer = document.querySelector('h1 span');
-const listItems = document.getElementsByClassName('task');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task'); //aktualizuje nam na żywo ale nie można forEach
 const input = document.querySelector('input');
 
+
+
 const removeTask = (e) => {
-    e.target.parentNode.remove();
-    taskNumer.textContent = listItems.length;
+    const index = e.target.parentNode.dataset.key; //pobieramy index tablicy dla usunięcia elementu
+    toDoList.splice(index, 1); //czyścumy index tablicy usuwanego elementu
+    console.log(toDoList)
+    taskNumber.textContent = toDoList.length;
+    renderList();
 }
 
+
+
 const addTask = (e) => {
-    e.preventDefault();
-    const titleTask = input.value;
-
-    if (titleTask === "") return;
-
+    e.preventDefault(); //nie odświeża strony
+    const titleTask = input.value; //pobieramy wpisywana wartość w input
+    if (titleTask === "") return; //blokowanie możliwości dodania pustego zadania
     const task = document.createElement('li');
-    task.className = "task";
-
-    task.innerHTML = titleTask + "<button> Usuń</button>"
+    task.className = "task"; //tworzenie listy zadań z klasami
+    task.innerHTML = titleTask + "<button> Usuń</button>"; //wpisujemy w li buttona
+    toDoList.push(task); //pushowanie do tablicy
+    renderList();
     ul.appendChild(task);
-    input.value = "";
+    input.value = ""; //czyszczenie inputa po wysłaniu
 
     // const liItems = document.querySelectorAll('li.task').length;
-    taskNumer.textContent = listItems.length;
+    taskNumber.textContent = listItems.length;
     task.querySelector('button').addEventListener('click', removeTask) //wykonujemy zadanie/poszukiwanie w tasku
     // document.querySelector('li:last-child button').addEventListener('click', removeTask) //druga wersja
 }
 
+
+const renderList = () => {
+    ul.textContent = "";
+    toDoList.forEach((toDoElement, key) => { //nadajemy numeracje/index dla każdego elementu pobieranego do tablicy
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement); //dodajemy index przy pomocy data-key
+    })
+}
 form.addEventListener('submit', addTask)
